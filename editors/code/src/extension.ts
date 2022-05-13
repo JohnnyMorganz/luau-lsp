6a3a3403cd25421d366bee7eb9328ad7395105f3
JohnnyMorganz <johnnymorganz@outlook.com>
2022-05-13 19:44:11 +0100

    Make extension use built version when running

--- editors/code/src/extension.ts
+++ editors/code/src/extension.ts
@@ -1,5 +1,6 @@
-import path = require("path");
 import * as vscode from "vscode";
+import * as os from "os";
+import * as fs from "fs";
 import {
   Executable,
   ServerOptions,
@@ -8,22 +9,54 @@ import {
   Trace,
 } from "vscode-languageclient/node";
 
+export const fileExists = (path: vscode.Uri | string): Thenable<boolean> => {
+  const uri = path instanceof vscode.Uri ? path : vscode.Uri.file(path);
+  return vscode.workspace.fs.stat(uri).then(
+    () => true,
+    () => false
+  );
+};
+
+const findBuiltServer = (context: vscode.ExtensionContext) => {
+  const platform = os.platform();
+  const path = vscode.Uri.joinPath(
+    context.extensionUri,
+    "bin",
+    platform,
+    `luau-lsp${platform === "win32" ? ".exe" : ""}`
+  );
+
+  //   if (!fileExists(path)) {
+  //     vscode.window.showErrorMessage("Luau LSP is currently not supported on your platform");
+  //     return undefined
+  //   }
+
+  if (platform !== "win32") {
+    fs.chmodSync(path.fsPath, "777");
+  }
+  return path;
+};
+
 export function activate(context: vscode.ExtensionContext) {
   console.log("Luau LSP activated");
 
-  const serverPath = vscode.Uri.joinPath(
-    context.extensionUri,
-    "..",
-    "..",
-    "build",
-    "Debug",
-    "luau-lsp.exe"
-  ).fsPath;
-
   const run: Executable = {
-    command: serverPath,
+    command: findBuiltServer(context).fsPath,
   };
-  const serverOptions: ServerOptions = { run, debug: run };
+
+  // If debugging, run the locally build extension
+  const debug: Executable = {
+    command: vscode.Uri.joinPath(
+      context.extensionUri,
+      "..",
+      "..",
+      "build",
+      "Debug",
+      "luau-lsp.exe"
+    ).fsPath,
+  };
+
+  const serverOptions: ServerOptions = { run, debug };
 
   const clientOptions: LanguageClientOptions = {
     documentSelector: [
