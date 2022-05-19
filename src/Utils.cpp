542fd94711db43a6f43f0a7eeba0c8943ee110e2
JohnnyMorganz <johnnymorganz@outlook.com>
2022-05-19 17:08:55 +0100

    Fix static

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "LSP/Utils.hpp"
 
-static std::optional<std::string> getParentPath(const std::string& path)
+std::optional<std::string> getParentPath(const std::string& path)
 {
     if (path == "" || path == "." || path == "/")
         return std::nullopt;
@@ -17,12 +17,12 @@ static std::optional<std::string> getParentPath(const std::string& path)
     return "";
 }
 
-static std::string codeBlock(std::string language, std::string code)
+std::string codeBlock(std::string language, std::string code)
 {
     return "```" + language + "\n" + code + "\n" + "```";
 }
 
-static std::optional<std::string> readFile(const std::filesystem::path& filePath)
+std::optional<std::string> readFile(const std::filesystem::path& filePath)
 {
     std::ifstream fileContents;
     fileContents.open(filePath);
@@ -42,18 +42,18 @@ static std::optional<std::string> readFile(const std::filesystem::path& filePath
     }
 }
 
-static void trim_start(std::string& str)
+void trim_start(std::string& str)
 {
     str.erase(0, str.find_first_not_of(" \n\r\t"));
 }
 
 
-static void trim_end(std::string& str)
+void trim_end(std::string& str)
 {
     str.erase(str.find_last_not_of(" \n\r\t") + 1);
 }
 
-static void trim(std::string& str)
+void trim(std::string& str)
 {
     trim_start(str);
     trim_end(str);
