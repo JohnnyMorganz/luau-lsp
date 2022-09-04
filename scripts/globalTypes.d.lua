8dc6ff430434a2210974de68b6dd19c97078ca5d
JohnnyMorganz <johnnymorganz@outlook.com>
2022-09-04 13:33:31 +0100

    Revert "Improve RequestAsync type (#121)"
    This reverts commit f8cc8e62c93689f2630fc2876c14c76711bdafa4.

--- scripts/globalTypes.d.lua
+++ scripts/globalTypes.d.lua
@@ -3544,21 +3544,6 @@ export type RBXScriptSignal<T... = ...any> = {
     ConnectParallel: (self: RBXScriptSignal<T...>, callback: (T...) -> ()) -> RBXScriptConnection,
 }
 
-type HttpRequestOptions = {
-    Url: string,
-    Method: "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH" | nil,
-    Headers: { [string]: string }?,
-    Body: string?,
-}
-
-type HttpResponseData = {
-    Success: boolean,
-    StatusCode: number,
-    StatusMessage: string,
-    Headers: { [string]: string },
-    Body: string?,
-}
-
 type Instance = any
 type Accoutrement = any
 type Accessory = any
@@ -6356,7 +6341,7 @@ declare class HttpService extends Instance
 	function UrlEncode(self, input: string): string
 	function GetAsync(self, url: string, nocache: boolean?, headers: any): string
 	function PostAsync(self, url: string, data: string, content_type: EnumHttpContentType?, compress: boolean?, headers: any): string
-	function RequestAsync(self, options: HttpRequestOptions): HttpResponseData
+	function RequestAsync(self, requestOptions: { [any]: any }): { [any]: any }
 end
 
 declare class Humanoid extends Instance
