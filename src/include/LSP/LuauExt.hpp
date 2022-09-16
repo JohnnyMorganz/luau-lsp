bbe1e3dc3d3a41160905183667a599822656151e
JohnnyMorganz <johnnymorganz@outlook.com>
2022-09-16 19:45:27 +0100

    Fix singletonTypes no longer global

--- src/include/LSP/LuauExt.hpp
+++ src/include/LSP/LuauExt.hpp
@@ -11,8 +11,8 @@ namespace types
 std::optional<Luau::TypeId> getTypeIdForClass(const Luau::ScopePtr& globalScope, std::optional<std::string> className);
 std::optional<std::string> getTypeName(Luau::TypeId typeId);
 
-Luau::TypeId makeLazyInstanceType(Luau::TypeArena& arena, const Luau::ScopePtr& globalScope, const SourceNodePtr& node,
-    std::optional<Luau::TypeId> parent, std::optional<Luau::TypeId> baseClass = std::nullopt);
+Luau::TypeId makeLazyInstanceType(const Luau::TypeChecker& typeChecker, Luau::TypeArena& arena, const Luau::ScopePtr& globalScope,
+    const SourceNodePtr& node, std::optional<Luau::TypeId> parent, std::optional<Luau::TypeId> baseClass = std::nullopt);
 
 // Magic function for `Instance:IsA("ClassName")` predicate
 std::optional<Luau::WithPredicate<Luau::TypePackId>> magicFunctionInstanceIsA(
