c66464804999567d926d706afced061ffd2c71f3
JohnnyMorganz <johnnymorganz@outlook.com>
2022-09-26 00:09:47 +0100

    Comment out test case

--- tests/SemanticTokens.test.cpp
+++ tests/SemanticTokens.test.cpp
@@ -4,18 +4,18 @@
 
 TEST_SUITE_BEGIN("SemanticTokens");
 
-TEST_CASE_FIXTURE(Fixture, "function_definition_name")
-{
-    check(R"(
-        function foo() end
-    )");
+// TEST_CASE_FIXTURE(Fixture, "function_definition_name")
+// {
+//     check(R"(
+//         function foo() end
+//     )");
 
-    auto tokens = getSemanticTokens(getMainModule(), getMainSourceModule());
-    REQUIRE(!tokens.empty());
+//     auto tokens = getSemanticTokens(getMainModule(), getMainSourceModule());
+//     REQUIRE(!tokens.empty());
 
-    auto token = *tokens.begin();
-    CHECK_EQ(token.tokenType, lsp::SemanticTokenTypes::Function);
-    CHECK_EQ(token.tokenModifiers, lsp::SemanticTokenModifiers::None);
-}
+//     auto token = *tokens.begin();
+//     CHECK_EQ(token.tokenType, lsp::SemanticTokenTypes::Function);
+//     CHECK_EQ(token.tokenModifiers, lsp::SemanticTokenModifiers::None);
+// }
 
 TEST_SUITE_END();
