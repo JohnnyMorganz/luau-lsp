40d061dda119ab35d39cd52382efdc093acd1982
Qualadore <me@qualadore.com>
2022-10-29 14:36:27 +0000

    Add `RBXScriptSignal:Once`

--- scripts/globalTypes.d.lua
+++ scripts/globalTypes.d.lua
@@ -3654,6 +3654,7 @@ export type RBXScriptSignal<T... = ...any> = {
     Wait: (self: RBXScriptSignal<T...>) -> T...,
     Connect: (self: RBXScriptSignal<T...>, callback: (T...) -> ()) -> RBXScriptConnection,
     ConnectParallel: (self: RBXScriptSignal<T...>, callback: (T...) -> ()) -> RBXScriptConnection,
+    Once: (self: RBXScriptSignal<T...>, callback: (T...) -> ()) -> RBXScriptConnection,
 }
 
 type HttpRequestOptions = {
