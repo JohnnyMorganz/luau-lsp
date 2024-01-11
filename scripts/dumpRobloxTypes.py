7be4965dc74e900b469f3bf7cfc4a45482d09b78
Max <clonet1019@gmail.com>
2024-01-10 20:03:34 -0600

    Support floor division in Vector2 & Vector3

--- scripts/dumpRobloxTypes.py
+++ scripts/dumpRobloxTypes.py
@@ -263,6 +263,7 @@ EXTRA_MEMBERS = {
         "function __sub(self, other: Vector3): Vector3",
         "function __mul(self, other: Vector3 | number): Vector3",
         "function __div(self, other: Vector3 | number): Vector3",
+        "function __idiv(self, other: Vector3 | number): Vector3",
         "function __unm(self): Vector3",
     ],
     "Vector2": [
@@ -270,6 +271,7 @@ EXTRA_MEMBERS = {
         "function __sub(self, other: Vector2): Vector2",
         "function __mul(self, other: Vector2 | number): Vector2",
         "function __div(self, other: Vector2 | number): Vector2",
+        "function __idiv(self, other: Vector2 | number): Vector2",
         "function __unm(self): Vector2",
     ],
     "Vector3int16": [
