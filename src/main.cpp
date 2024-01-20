f01ac2c31048f93a1f8c6a5fe83fa3f24a55f09c
JohnnyMorganz <johnnymorganz@outlook.com>
2024-01-20 12:53:14 +0100

    v1.27.1 - fix versions

--- src/main.cpp
+++ src/main.cpp
@@ -192,7 +192,7 @@ int main(int argc, char** argv)
         return 1;
     };
 
-    argparse::ArgumentParser program("luau-lsp", "1.27.0");
+    argparse::ArgumentParser program("luau-lsp", "1.27.1");
     program.set_assign_chars(":=");
 
     // Global arguments
