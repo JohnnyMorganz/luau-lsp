20c47981e04c770ec431f9e788fd42f420ebafd9
JohnnyMorganz <johnnymorganz@outlook.com>
2024-11-16 15:30:05 +0100

    i figured it out...
    Luau::parseConfig crashes if configLocation is an empty string

--- tests/Fixture.cpp
+++ tests/Fixture.cpp
@@ -167,8 +167,8 @@ void Fixture::loadSourcemap(const std::string& contents)
 
 void Fixture::loadLuaurc(const std::string& source)
 {
-    REQUIRE(
-        !WorkspaceFileResolver::parseConfig(std::filesystem::path() / Luau::kConfigName, source, workspace.fileResolver.defaultConfig).has_value());
+    REQUIRE(!WorkspaceFileResolver::parseConfig(std::filesystem::current_path() / Luau::kConfigName, source, workspace.fileResolver.defaultConfig)
+                 .has_value());
 }
 
 SourceNodePtr Fixture::getRootSourceNode()
