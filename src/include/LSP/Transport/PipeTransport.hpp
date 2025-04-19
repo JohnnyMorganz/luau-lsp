fd023ae15538819d1eeff12d12e315fc8bac0001
JohnnyMorganz <johnnymorganz@outlook.com>
2025-04-19 18:08:48 +0200

    Support pipe communication (#1005)
    * Support pipe communication
    
    * Don't compile PipeTransport on windows
    
    * Fix test compilation
    
    * Update changelog

---
+++ src/include/LSP/Transport/PipeTransport.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Transport.hpp"
+
+class PipeTransport : public Transport {
+public:
+    PipeTransport(std::string socketPath);
+    ~PipeTransport() override;
+
+    void send(const std::string& data) override;
+    void read(char *buffer, unsigned int length) override;
+    bool readLine(std::string& output) override;
+
+private:
+    std::string socketPath;
+    int socketFd = -1;
+
+    void connect();
+};
