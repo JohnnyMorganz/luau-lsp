9f29516acb64e4e629f0d9307ed51a795bd9a196
JohnnyMorganz <johnnymorganz@outlook.com>
2025-12-25 18:34:35 +0000

    Add InlayHintPart definition

--- src/include/Protocol/LanguageFeatures.hpp
+++ src/include/Protocol/LanguageFeatures.hpp
@@ -133,6 +133,51 @@ enum struct InlayHintKind
     Parameter = 2,
 };
 
+/**
+ * An inlay hint label part allows for interactive and composite labels
+ * of inlay hints.
+ *
+ * @since 3.17.0
+ */
+struct InlayHintLabelPart
+{
+    /**
+     * The value of this label part.
+     */
+    std::string value;
+
+    /**
+     * The tooltip text when you hover over this label part. Depending on
+     * the client capability `inlayHint.resolveSupport` clients might resolve
+     * this property late using the resolve request.
+     */
+    std::optional<lsp::MarkupContent> tooltip = std::nullopt;
+
+    /**
+     * An optional source code location that represents this
+     * label part.
+     *
+     * The editor will use this location for the hover and for code navigation
+     * features: This part will become a clickable link that resolves to the
+     * definition of the symbol at the given location (not necessarily the
+     * location itself), it shows the hover that shows at the given location,
+     * and it shows a context menu with further code navigation commands.
+     *
+     * Depending on the client capability `inlayHint.resolveSupport` clients
+     * might resolve this property late using the resolve request.
+     */
+    std::optional<lsp::Location> location = std::nullopt;
+
+    /**
+     * An optional command for this label part.
+     *
+     * Depending on the client capability `inlayHint.resolveSupport` clients
+     * might resolve this property late using the resolve request.
+     */
+    std::optional<lsp::Command> command = std::nullopt;
+};
+NLOHMANN_DEFINE_OPTIONAL(InlayHintLabelPart, value, tooltip, location, command)
+
 struct InlayHint
 {
     Position position;
