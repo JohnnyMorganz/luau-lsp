5b04ad00990e2c429c06090c85f3a068064d796b
JohnnyMorganz <johnnymorganz@outlook.com>
2026-01-04 14:39:05 +0100

    Fix

--- src/include/Protocol/FileOperations.hpp
+++ src/include/Protocol/FileOperations.hpp
@@ -56,7 +56,7 @@ struct FileOperationPattern
 	 * - `*` to match zero or more characters in a path segment
 	 * - `?` to match on one character in a path segment
 	 * - `**` to match any number of path segments, including none
-	 * - `{}` to group sub patterns into an OR expression. (e.g. `**​/*.{ts,js}`
+	 * - `{}` to group sub patterns into an OR expression. (e.g. `*.{ts,js}`
 	 *   matches all TypeScript and JavaScript files)
 	 * - `[]` to declare a range of characters to match in a path segment
 	 *   (e.g., `example.[0-9]` to match on `example.0`, `example.1`, …)
