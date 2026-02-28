22988f37a9e70e633a3736e0c78b59008c8d7189
Abby <eternalabby@proton.me>
2026-02-28 16:56:07 -0400

    Improvements for extracting @luau documentation
    Since it's harder to correctly determine where the functions are on the Luau site; they are put into block quotes, this change only changes the URL's fragment so that it directs to the library's section.
    
    - Changed links directing to the roblox docs site to the luau docs.
    - Import only the necessary methods.
    - Pretty-print the JSON file.
    - Explicitly close files after using them.

--- scripts/extractLuauDocs.py
+++ scripts/extractLuauDocs.py
@@ -1,9 +1,38 @@
-import json
+from json import load, dump
 
 with open("api-docs/en-us.json") as f:
-    data = json.load(f)
+    data: dict[str, dict[str, str]] = load(f)
+    f.close()
 
 luau_docs = {k: v for k, v in data.items() if k.startswith("@luau")}
 
 with open("api-docs/luau-en-us.json", "w") as f:
-    json.dump(luau_docs, f)
+    
+    # changing link from the roblox docs site to luau docs site.
+    for id, docs_entry in luau_docs.items():
+        link = docs_entry.get("learn_more_link")
+        
+        if link is None:
+            continue
+        
+        # if the roblox docs url is to a built-in library
+        if link.startswith("https://create.roblox.com/docs/reference/engine/libraries/"):
+            
+            # find the index of the path separator ("/") and the fragment ("#")
+            path_sep_idx = link.rfind("/")
+            fragment_idx = link.rfind("#")
+            
+            # fragment index is corrected whenever finding the symbol fails
+            fragment_idx = None if fragment_idx == -1 else fragment_idx + 1
+            
+            # add missing fragment symbol ("#") because the roblox docs link doesn't have it
+            link = link[:path_sep_idx + 1] + "#" + link[path_sep_idx + 1:]
+            
+            # add to the computed dictionary, append "-library" because the luau site requires it
+            luau_docs[id]["learn_more_link"] = "https://luau.org/library/" + link[path_sep_idx + 1:fragment_idx] + "-library"
+            
+        # the only other case is that the url is to a global function
+        else:
+            luau_docs[id]["learn_more_link"] = "https://luau.org/library/#global-functions"
+    dump(luau_docs, f, indent=4)
+    f.close()
