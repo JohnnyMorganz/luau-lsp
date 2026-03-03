dec03a05682e1807a2309fc3a44f9769eac98155
Abby <eternalabby@proton.me>
2026-03-03 10:57:33 -0400

    small change
    - Moved code for URL change.
    - Improved code comments.
    - Changed indent to 2 spaces to match `en-us.json`

--- scripts/extractLuauDocs.py
+++ scripts/extractLuauDocs.py
@@ -6,33 +6,35 @@ with open("api-docs/en-us.json") as f:
 
 luau_docs = {k: v for k, v in data.items() if k.startswith("@luau")}
 
-with open("api-docs/luau-en-us.json", "w") as f:
+# Changes the URL from directing to the roblox docs site to the luau.org site.
+for id, docs_entry in luau_docs.items():
+    link = docs_entry.get("learn_more_link")
     
-    # changing link from the roblox docs site to luau docs site.
-    for id, docs_entry in luau_docs.items():
-        link = docs_entry.get("learn_more_link")
+    if link is None:
+        continue
+    
+    # If the URL is from a built-in library.
+    if link.startswith("https://create.roblox.com/docs/reference/engine/libraries/"):
         
-        if link is None:
-            continue
+        # Find the index from the right of the path separator ("/") and the fragment ("#")
+        path_sep_idx = link.rfind("/")
+        fragment_idx = link.rfind("#")
         
-        # if the roblox docs url is to a built-in library
-        if link.startswith("https://create.roblox.com/docs/reference/engine/libraries/"):
-            
-            # find the index of the path separator ("/") and the fragment ("#")
-            path_sep_idx = link.rfind("/")
-            fragment_idx = link.rfind("#")
-            
-            # fragment index is corrected whenever finding the symbol fails
-            fragment_idx = None if fragment_idx == -1 else fragment_idx + 1
-            
-            # add missing fragment symbol ("#") because the roblox docs link doesn't have it
-            link = link[:path_sep_idx + 1] + "#" + link[path_sep_idx + 1:]
-            
-            # add to the computed dictionary, append "-library" because the luau site requires it
-            luau_docs[id]["learn_more_link"] = "https://luau.org/library/" + link[path_sep_idx + 1:fragment_idx] + "-library"
-            
-        # the only other case is that the url is to a global function
-        else:
-            luau_docs[id]["learn_more_link"] = "https://luau.org/library/#global-functions"
-    dump(luau_docs, f, indent=4)
+        # Corrects the fragment index if the fragment is found.
+        # This is only the case for directing to specific functions.
+        fragment_idx = None if fragment_idx == -1 else fragment_idx + 1
+        
+        # Add the missing fragment symbol ("#") because the roblox docs link doesn't have it,
+        # while the luau.org link does.
+        link = link[:path_sep_idx + 1] + "#" + link[path_sep_idx + 1:]
+        
+        # Add formats the new link.
+        luau_docs[id]["learn_more_link"] = "https://luau.org/library/" + link[path_sep_idx + 1:fragment_idx] + "-library"
+        
+    # Otherwise, it's from a global function.
+    else:
+        luau_docs[id]["learn_more_link"] = "https://luau.org/library/#global-functions"
+
+with open("api-docs/luau-en-us.json", "w") as f:
+    dump(luau_docs, f, indent=2)
     f.close()
