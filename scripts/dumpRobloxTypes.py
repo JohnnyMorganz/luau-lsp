0ae78f428c667e5feaa6d7fd7e68356aa46c69a3
JohnnyMorganz <johnnymorganz@outlook.com>
2026-06-28 16:32:43 +0200

    Fix ReflectionService method signatures and type stub aliases (#1553)

--- scripts/dumpRobloxTypes.py
+++ scripts/dumpRobloxTypes.py
@@ -79,6 +79,19 @@ LUAU_SNIPPET_PATCHES = {
 
     "declare Workspace: any": "",
     "declare Game: any": "",
+
+    "type ReflectedClassOrNil = any": "type ReflectedClassOrNil = ReflectedClass?",
+    "type ReflectedClasses = any": "type ReflectedClasses = { ReflectedClass }",
+    "type ReflectedProperties = any": "type ReflectedProperties = { ReflectedProperty }",
+
+    "type ReflectionClassFilter = {": "export type ReflectionClassFilter = {",
+    "type ReflectionMemberFilter = {": "export type ReflectionMemberFilter = {",
+    "type ReflectionType = {": "export type ReflectionType = {",
+    "type ReflectionParameter = {": "export type ReflectionParameter = {",
+    "type ReflectedProperty = {": "export type ReflectedProperty = {",
+    "type ReflectedMethod = {": "export type ReflectedMethod = {",
+    "type ReflectedEvent = {": "export type ReflectedEvent = {",
+    "type ReflectedClass = {": "export type ReflectedClass = {",
 }
 
 TYPE_INDEX = {
@@ -1262,6 +1275,10 @@ def applyCorrections(dump: ApiDump, corrections: CorrectionsDump):
                                     otherMember["ReturnType"]["Generic"] = member[
                                         "ReturnType"
                                     ]["Generic"]
+                                if "Declared" in member["ReturnType"]:
+                                    otherMember["ReturnType"]["Declared"] = member[
+                                        "ReturnType"
+                                    ]["Declared"]
                             elif "ValueType" in member:
                                 otherMember["ValueType"]["Name"] = (
                                     member["ValueType"]["Name"]
@@ -1289,6 +1306,10 @@ def applyCorrections(dump: ApiDump, corrections: CorrectionsDump):
                                                 if "Generic" in param["Type"]:
                                                     otherParam["Type"]["Generic"] = (
                                                         param["Type"]["Generic"]
+                                                    )
+                                                if "Declared" in param["Type"]:
+                                                    otherParam["Type"]["Declared"] = (
+                                                        param["Type"]["Declared"]
                                                     )
                                             if "Default" in param:
                                                 otherParam["Default"] = param["Default"]
